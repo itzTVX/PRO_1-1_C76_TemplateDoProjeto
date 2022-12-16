b13060e58ab5fde38f9260db0a59f31e266996ab
itzTVX <94137584+itzTVX@users.noreply.github.com>
2022-12-16 16:35:58 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 //import { StackNavigator } from '@react-navigation/stack';
-//import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 //import { createStackNavigation } from '@react-navigation/stack';
 //import { createStack } from '@react-navigation/stack';
 
@@ -20,10 +20,10 @@ function App() {
         headerShown: false
       }}>
     
-        {/* <Stack.Screen component="Home" name={HomeScreen} /> */}
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        {/* <Stack.Screen name="Home" component="HomeScreen" /> */}
-        {/* <Stack.Screen name:"Home" component:{HomeScreen} /> */}
+        {/* <Stack.Screen component="Home" name={HomeScreen} > */}
+        {* <Stack.Screen name="Home" component={HomeScreen} > */}
+        {/* <Stack.Screen name="Home" component="HomeScreen" > */}
+        {/* <Stack.Screen name:"Home" component:{HomeScreen} > */}
     
         <Stack.Screen name="StarMap" component={StartMapScreen} />
         <Stack.Screen name="DailyPic" component={DailyPicScreen} />
